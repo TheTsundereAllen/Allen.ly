20d018e6b9d05613916be757764f48c5f9a19fef
Allen Zhang <1124796703@qq.com>
2020-01-07 15:56:57 -0800

    kind of finished backend

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hash = require("./util/hash");
+var urlRegex = require('url-regex');
 
 var app = express();
 var MongoClient = require("mongodb").MongoClient;
@@ -31,65 +32,113 @@ ConnectionURL = ConnectionURL.replace("{host}", "173.230.155.191")
 MongoClient.connect(
     ConnectionURL,
     function (err, client) {
-      var db = client.db(Database).admin();
+      var db = client.db(Database);
       app.locals.db = db;
+
+      console.log("Connected");
 
       var collection = db.collection("data");
 
+      app.get("/", function (req, res) {
+          res.status(200);
+          res.render("./public/index.html");
+      });
+
       app.get("/:id", function(req, res) {
-          collection.find({
-              "shortened-id": req.params.id.toLowerCase()
-          }).toArray(function (err, result) {
-              if (err != null) {
-                  res.status(404);
-              } else {
-                  var url = result[0]["original-url"];
-                  if (url == null) {
+          if (req.params.length > 0) {
+              collection.find({
+                  "shortened-id": req.params.id.toLowerCase()
+              }).toArray(function (err, result) {
+                  if (err != null) {
                       res.status(404);
-                      res.render("./public/404.html") //For example
                   } else {
-                      res.redirect(301, url);
+                      var url = result[0]["original-url"];
+                      if (url == null) {
+                          res.status(404);
+                          res.render("./public/404.html") //For example
+                      } else {
+                          res.redirect(301, url);
+                      }
                   }
-              }
-          })
+              })
+          }
       });
 
       app.post("/api/shorten-url", (function (req, res) {
           var originalURL = req.body["original-url"];
-          var urlId = req.body["customized-id"];
 
-          if (urlId == null) {
-              var hashedURL = hash.hash(originalURL);
-              var rangeMin = Math.random() * (hashedURL.length - 7);
-              urlId = hashedURL.substring(rangeMin, rangeMin + 7);
+          if (!urlRegex({
+              exact: true
+          }).test(originalURL)) {
+            res.status(400);
+            res.send({
+              code: 400,
+              message: 'Bad Request',
+              description: 'The URL specified is not a valid link'
+            })
           } else {
-              (collection.find({
-                  "url-id": urlId
-              }), function (err, result) {
-                  if (result != null) {
-                      urlId = null;
+              var urlId = req.body["customized-id"];
+
+              if (urlId == null) {
+                  var hashedURL = hash.hash(originalURL);
+                  var rangeMin = Math.random() * (hashedURL.length - 7);
+                  urlId = hashedURL.substring(rangeMin, rangeMin + 7);
+              } else {
+                  if (collection.find({
+                    "shortened-id": urlId.trim()
+                  })) {
+                      res.status(400);
+                      res.send({
+                          code: 400,
+                          message: 'Bad Request',
+                          description: 'The specified id already exists'
+                      })
                   }
-              });
-          }
+              }
 
+              while (collection.find({
+                  "shortened-id": urlId.trim()
+              })) {
+                  hashedURL = hash.hash(originalURL);
+                  rangeMin = Math.random() * (hashedURL.length - 7);
+                  urlId = hashedURL.substring(rangeMin, rangeMin + 7);
+              }
 
-          var document = {
-              "original-url": originalURL,
-              "shortened-id": urlId
-          };
+              var document = {
+                  "original-url": originalURL,
+                  "shortened-id": urlId.trim()
+              };
 
-          if (urlId != null) {
-              collection.insertOne(document, {}, function (err, result) {
-                  if (err != null) {
-                      res.status(500);
-                      return;
-                  }
+              if (urlId != null) {
+                  collection.insertOne(document, {}, function (err, result) {
+                      if (err != null) {
+                          res.status(500);
+                          res.send({
+                             code: 500,
+                             message: 'Internal Server Error',
+                             description: 'An internal server error has occurred, please try again or report it to the administrator'
+                          });
+                          return;
+                      }
 
-                  res.status(200);
-                  console.log("Document inserted");
-              });
-          } else {
-              res.status(402);
+                      res.status(200);
+                      res.send({
+                          code: 200,
+                          message: 'Execution Successful',
+                          description: 'The URL has been successfully shortened and uploaded to the database',
+                          original_url: document["original-url"],
+                          shortened_id: document["shortened-id"]
+                      });
+                      console.log("Document inserted");
+                  });
+              } else {
+                  res.status(400);
+                  res.send({
+                      code: 400,
+                      message: 'Bad Request',
+                      description: 'The ID of the URL is missing during the execution process'
+                  })
+              }
           }
 
       }));
