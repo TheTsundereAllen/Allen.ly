d85408851d0af2a3653cc15fc72e8e49e056208c
lillee205 <leelillian205@gmail.com>
2020-01-07 16:53:33 -0800

    basic html done

---
+++ public/index.js
@@ -0,0 +1,8 @@
+document.getElementById("generateBtn").addEventListener("click",function(){
+  document.getElementById("modal").style.display="inline-block";
+  document.getElementById("blackmodal").style.display="block";
+})
+document.getElementById("deletebutton").addEventListener("click",function(){
+  document.getElementById("modal").style.display="none";
+  document.getElementById("blackmodal").style.display="none";
+})
